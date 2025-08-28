4695e265a6969b4aee275b686f5a9d413be80ca2
marcoswv12 <marcos.wv122@gmail.com>
2025-08-28 14:50:58 -0300

    Update Program.cs

--- Calculadora-ColaborativaC#/Program.cs
+++ Calculadora-ColaborativaC#/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static float Resultado = 0;
+    static float a = 0;
+    static float b = 0;
+
+    public static void Main(string[] args)
+        {
+        a = float.Parse(Console.ReadLine());
+        char operação = char.Parse(Console.ReadLine()); 
+        b = float.Parse(Console.ReadLine());
+        
+        if(operação == '+')
+        {
+            soma();
+            Console.WriteLine(Resultado);
+        }
+        else if(operação == '-')
+        {
+            subtração();
+            Console.WriteLine(Resultado);
+        }
+        else
+        {
+            Console.WriteLine("Operação invalida.");
+            return;
+        }
+       
+    }
+    
+  static void soma()
+  {
+
+  }
+    static void subtração()
+  { 
+
+  }
+}
+
